08a30808685efb01b8b76ba9f07c330e8376af74
Filipe <Filipecasagrande1@gmail.com>
2025-04-08 20:47:42 -0300

    Implementado estrutura para certifricar que o número é impar

--- Trabalho 01 - Diamante de X/Program.cs
+++ Trabalho 01 - Diamante de X/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            int x;
+            Console.WriteLine("Informe um número impar para determinar a quantidade de X do diamante");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            while (x % 2 == 0)
+            {
+                Console.WriteLine("Entrada inválida, informe um número impar");
+                x = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
