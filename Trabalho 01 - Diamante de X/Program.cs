79c6a1a59b987e318c015ba6d759f69583b9cf3e
Filipe <Filipecasagrande1@gmail.com>
2025-04-09 21:41:21 -0300

    Implementado: Parte superior e inferior do diamante

--- Trabalho 01 - Diamante de X/Program.cs
+++ Trabalho 01 - Diamante de X/Program.cs
@@ -7,14 +7,41 @@ namespace Trabalho_01___Diamante_de_X
     {
         static void Main(string[] args)
         {
-            int n = entradaDeDados();
-
-            
+            int n = EntradaDeDados();
 
             int espacos = n / 2;
             int letras = 1;
             int meio = n / 2;
+            
+            ParteSuperior(espacos, letras, meio);
 
+            espacos = 1;
+            letras = n - 2;
+
+            ParteInferior(espacos, letras, meio);
+
+            Console.ReadLine();
+        }
+        static int EntradaDeDados()
+        {
+            int x;
+
+            Console.Write("Informe um número impar para determinar a quantidade de X do diamante: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Verificacao(x);
+            return x;
+        }
+        static void Verificacao(int x)
+        {
+            while (x % 2 == 0 || x < 1)
+            {
+                Console.WriteLine("Entrada inválida, informe um número impar positivo");
+                x = Convert.ToInt32(Console.ReadLine());
+
+            }
+        }
+        static void ParteSuperior(int espacos, int letras, int meio)
+        {
             for (int i = 0; i <= meio; i++)
             {
                 for (int j = 0; j < espacos; j++)
@@ -30,10 +57,9 @@ namespace Trabalho_01___Diamante_de_X
                 espacos--;
                 letras += 2;
             }
-
-            espacos = 1;
-            letras = n - 2;
-
+        }
+        static void ParteInferior(int espacos, int letras, int meio)
+        {
             for (int i = 0; i < meio; i++)
             {
                 for (int j = 0; j < espacos; j++)
@@ -48,26 +74,6 @@ namespace Trabalho_01___Diamante_de_X
                 Console.WriteLine();
                 espacos++;
                 letras -= 2;
-            }
-
-            Console.ReadLine();
-        }
-        static int entradaDeDados()
-        {
-            int x;
-
-            Console.Write("Informe um número impar para determinar a quantidade de X do diamante: ");
-            x = Convert.ToInt32(Console.ReadLine());
-            verificacao(x);
-            return x;
-        }
-        static void verificacao(int x)
-        {
-            while (x % 2 == 0 || x < 1)
-            {
-                Console.WriteLine("Entrada inválida, informe um número impar positivo");
-                x = Convert.ToInt32(Console.ReadLine());
-
             }
         }
         
